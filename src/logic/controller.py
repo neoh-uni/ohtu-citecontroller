f2625cb84b5499ad0084e38d49c12d422420a1af
Joonas Ylönen <KA0Screativity@gmail.com>
2022-11-26 00:05:11 +0200

    Add functionality for validating book reference

--- src/logic/controller.py
+++ src/logic/controller.py
@@ -1,17 +1,45 @@
 from reference import Book
 
+class UserInputError(Exception):
+    pass
+
 class Controller:
 
-    def validate(type: str, info: dict):
-        # todo
+    def __init__(self):
         pass
+
+    def validate_book(info: dict):
+        if "author" not in info:
+            raise UserInputError("Book must have an author")
+
+        if info["author"] == None:
+            raise UserInputError("Book must have an author")
+
+        if "title" not in info:
+            raise UserInputError("Book must have a title")
+
+        if info["title"] == None:
+            raise UserInputError("Book must have a title")
+
+        if "year" not in info:
+            raise UserInputError("Book must have a year")
+
+        if info["year"] == None:
+            raise UserInputError("Book must have a year")
+
+        if "publisher" not in info:
+            raise UserInputError("Book must have a publisher")
+
+        if info["publisher"] == None:
+            raise UserInputError("Book must have a publisher")
+
 
     def compile_book(info: dict):
         book = Book(info["author"], info["title"], info["year"], info["publisher"])
 
         return book
 
-    def add_cite(type: str):
-        info = {}
+    def add_cite(type: str, info: dict):
         if type == "book":
+            validate_book(info)
             book = compile_book(info)
