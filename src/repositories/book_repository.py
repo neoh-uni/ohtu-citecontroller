f29fc3e1f83534f43f85d88e962aeb50d230dcc6
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2022-11-27 08:48:19 +0200

    update book_repository

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -1,16 +1,12 @@
 from app import db
-#from logic.reference import Book
+
 
 class BookRepository:
     def new_book(self, author, title, year, publisher):
-        # tähän Book
         sql = """INSERT INTO books (author, title, year, publisher)
                  VALUES (:author, :title, :year, :publisher)"""
         db.session.execute(sql, {"author":author, "title":title, "year":year, "publisher":publisher})
         db.session.commit()
-    
-    def get_books():
-        sql = "SELECT author, title, year, publisher FROM books"
-        return db.session.execute(sql).fetchall()
+
 
 book_repository = BookRepository()
