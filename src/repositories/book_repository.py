b146e66d9e769744b6d7af87f5018d02f81754d8
Alexander <ale.engelhardt@outlook.com>
2022-11-29 13:32:51 +0200

    refactor book_repository, book_service, controller removed
    Co-authored-by: neoh1
    Co-authored-by: JuusoSaavalainen
    Co-authored-by: meeries
    Co-authored-by: KA0Sgames
    Co-authored-by: Catrovitch

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -2,12 +2,13 @@ from app import db
 
 
 class BookRepository:
-    def new_book(self, author, title, year, publisher):
+    def new_book(self, book):
+        
         sql = """INSERT INTO books (author, title, year, publisher)
                  VALUES (:author, :title, :year, :publisher)"""
         db.session.execute(
             sql,
-            {"author": author, "title": title, "year": year, "publisher": publisher},
+            {"author": book.author, "title": book.title, "year": book.year, "publisher": book.publisher},
         )
         db.session.commit()
 
--- src/services/book_service.py
+++ src/services/book_service.py
@@ -1,15 +1,24 @@
 from repositories.book_repository import book_repository as default_book_repository
-from logic.controller import Controller
+from logic import reference
 
 class BookService:
     def __init__(self, book_repository=default_book_repository):
         self._book_repository = book_repository
-        self.c = Controller()
-
+        
 
     def add_book(self, author, title, year, publisher):
         # tähän Book
-        return self._book_repository.new_book(self.c.add_cite("book", {"author": author, "title": title, "year": year, "publisher": publisher}))
+        e = "book added"
+
+        try:
+            book = reference.Book(author, title, year, publisher)
+            self._book_repository.new_book(book)
+        except Exception as e:
+            print(e)
+        
+        return e
+
 
 
 book_s = BookService()
+
