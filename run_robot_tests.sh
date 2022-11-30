3ce4d003fd7bf2224a9175351572ee43b4418473
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-30 12:50:17 +0200

    fix typos

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -9,7 +9,7 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "2
 done
 
 # suoritetaan testit
-# TODO: poetry run robot src/e2e
+# TODO: poetry run robot src/tests
 
 status=$?
 
