9798639126a64301351e733e439a2429f520f177
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-30 12:14:32 +0200

    routes year

--- src/routes.py
+++ src/routes.py
@@ -18,6 +18,7 @@ def create_book():
     if request.method == "POST":
         title = request.form["book_name"]
         author = request.form["author"]
+        year = request.form["year"]
         publisher = request.form["publisher"]
         book_service.add_book(author, title, year, publisher)
         return redirect("/")
