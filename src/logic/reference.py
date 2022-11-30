acdf42955aad3bc3547cd90178c1802c4fee771d
Niklas Harju <niklas.harju@helsinki.fi>
2022-11-30 14:02:50 +0200

    Added support for Article, Inproceedings

--- src/logic/reference.py
+++ src/logic/reference.py
@@ -1,35 +1,9 @@
 """
-Reference types given by ohtu:
-
-@inproceedings{VPL11,
-    author = {Vihavainen, Arto and Paksula, Matti and Luukkainen, Matti},
-    title = {Extreme Apprenticeship Method in Teaching Programming for Beginners.},
-    year = {2011},
-    booktitle = {SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education},
-}
-
-@article{CBH91,
-    author = {Allan Collins and John Seely Brown and Ann Holum},
-    title = {Cognitive apprenticeship: making thinking visible},
-    journal = {American Educator},
-    year = {1991},
-    volume = {6},
-    pages = {38--46}
-}
-
-@book{Martin09,
-    author = {Martin, Robert},
-    title = {Clean Code: A Handbook of Agile Software Craftsmanship},
-    year = {2008},
-    publisher = {Prentice Hall},
-}
-
-
-More types: wikipedia https://en.wikibooks.org/wiki/LaTeX/Bibliography_Management
+Author: Niklas
+See from types: wikipedia https://en.wikibooks.org/wiki/LaTeX/Bibliography_Management
 e.g. booklet, inbook, incollection, manual, mastersthesis/phdhesis, misc, proceedings, tech report, unpublished
-
 """
-from attrs import define, astuple, validators, fields, field
+from attrs import define, validators, field
 from typing import Optional
 from datetime import datetime
 
@@ -64,24 +38,89 @@ def check_len(instance, attribute, given_str):
     BibTeX has a 5000 character limit for each field?
     e.g. https://clemson.libguides.com/LaTeX
     """
-    if len(given_str) > 5000:
+    if given_str is not None and len(given_str) > 5000:
         raise ValueError("Str length exceeds BibTeX field character limit of 5000")
+
+
+def check_str(instance, attribute, given_str):
+    if given_str is not None and not isinstance(given_str, str):
+        raise ValueError("Given value is not a string.")
 
 
 @define
 class Book:
+    """
+    @book{Martin09,
+        author = {Martin, Robert},
+        title = {Clean Code: A Handbook of Agile Software Craftsmanship},
+        year = {2008},
+        publisher = {Prentice Hall},
+    }
+    """
 
     author: str = field(validator=[validators.instance_of(str), check_name, check_len])
     title: str = field(validator=[validators.instance_of(str), check_len])
     year: int = field(converter=int, validator=[validators.instance_of(int), check_year])
     publisher: str = field(validator=[validators.instance_of(str), check_len])
 
-    # TODO: -- optional Bibitex settings for Book class, no checks yet --
-    address: Optional[str] = field(default=None)
-    edition: Optional[str] = field(default=None)
-    editor: Optional[str] = field(default=None)
-    month: Optional[str] = field(default=None)
-    note: Optional[str] = field(default=None)
-    number: Optional[str] = field(default=None)
-    series: Optional[str] = field(default=None)
-    volume: Optional[str] = field(default=None)
+    address: Optional[str] = field(default=None, validator=[check_str, check_len])
+    edition: Optional[str] = field(default=None, validator=[check_str, check_len])
+    editor: Optional[str] = field(default=None, validator=[check_str, check_len])
+    month: Optional[str] = field(default=None, validator=[check_str, check_len])
+    note: Optional[str] = field(default=None, validator=[check_str, check_len])
+    number: Optional[str] = field(default=None, validator=[check_str, check_len])
+    series: Optional[str] = field(default=None, validator=[check_str, check_len])
+    volume: Optional[str] = field(default=None, validator=[check_str, check_len])
+
+
+@define
+class Article:
+    """
+    @article{CBH91,
+        author = {Allan Collins and John Seely Brown and Ann Holum},
+        title = {Cognitive apprenticeship: making thinking visible},
+        journal = {American Educator},
+        year = {1991},
+        volume = {6},
+        pages = {38--46}
+    }
+    """
+
+    author: str = field(validator=[validators.instance_of(str), check_name, check_len])
+    journal: str = field(validator=[validators.instance_of(str), check_len])
+    title: str = field(validator=[validators.instance_of(str), check_len])
+    year: int = field(converter=int, validator=[validators.instance_of(int), check_year])
+
+    month: Optional[str] = field(default=None, validator=[check_str, check_len])
+    note: Optional[str] = field(default=None, validator=[check_str, check_len])
+    number: Optional[str] = field(default=None, validator=[check_str, check_len])
+    pages: Optional[str] = field(default=None, validator=[check_str, check_len])
+    volume: Optional[str] = field(default=None, validator=[check_str, check_len])
+
+
+@define
+class Inproceedings:
+    """
+    @inproceedings{VPL11,
+        author = {Vihavainen, Arto and Paksula, Matti and Luukkainen, Matti},
+        title = {Extreme Apprenticeship Method in Teaching Programming for Beginners.},
+        year = {2011},
+        booktitle = {SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education},
+    }
+    """
+
+    author: str = field(validator=[validators.instance_of(str), check_name, check_len])
+    title: str = field(validator=[validators.instance_of(str), check_len])
+    year: int = field(converter=int, validator=[validators.instance_of(int), check_year])
+    booktitle: str = field(validator=[validators.instance_of(str), check_len])
+
+    address: Optional[str] = field(default=None, validator=[check_str, check_len])
+    editor: Optional[str] = field(default=None, validator=[check_str, check_len])
+    month: Optional[str] = field(default=None, validator=[check_str, check_len])
+    note: Optional[str] = field(default=None, validator=[check_str, check_len])
+    number: Optional[str] = field(default=None, validator=[check_str, check_len])
+    organization: Optional[str] = field(default=None, validator=[check_str, check_len])
+    pages: Optional[str] = field(default=None, validator=[check_str, check_len])
+    publisher: Optional[str] = field(default=None, validator=[check_str, check_len])
+    series: Optional[str] = field(default=None, validator=[check_str, check_len])
+    volume: Optional[str] = field(default=None, validator=[check_str, check_len])
